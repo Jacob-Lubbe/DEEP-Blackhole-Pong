8d9661b758bae8b65a0e0bfd906535174570ccce
Jacob-Lubbe <118573254+Jacob-Lubbe@users.noreply.github.com>
2023-03-23 16:51:14 -0500

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+g_constant = 1.6e7
+mass_constant = 1
+null_vector = np.array((0.0,0.0))
+
+
+FPS = 600
+VEL = 5
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 125, 125
+
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+WIDTH, HEIGHT = 1280, 720
